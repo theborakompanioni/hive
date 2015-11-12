cb5ec99697ec1774347aa5c46849a91327b7131a
theborakompanioni <theborakompanioni+github@gmail.com>
2015-11-12 15:49:10 +0100

    remove public folder

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ fs.readdirSync(__dirname + '/models').forEach(function (file) {
 // configure express app
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/client/apps/chesshive/dist/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
